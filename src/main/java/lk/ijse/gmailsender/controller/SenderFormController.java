edadf9244cc4439ec484685f290268433bab1ce8
SavindaJayasekara <thantrige32@gmail.com>
2023-07-16 21:14:17 +0530

    add propertis

--- src/main/java/lk/ijse/gmailsender/controller/SenderFormController.java
+++ src/main/java/lk/ijse/gmailsender/controller/SenderFormController.java
@@ -1,4 +1,24 @@
 package lk.ijse.gmailsender.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class SenderFormController {
+    public TextArea areaEmails;
+    public TextArea areaSendFinish;
+    public Button btnStart;
+    public TextArea areaMsg;
+    public Button btnCheck;
+
+    String [] split;
+
+    public void btnStartOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnCheckOnAction(ActionEvent actionEvent) {
+        split = areaEmails.getText().split("\n");
+
+    }
 }
